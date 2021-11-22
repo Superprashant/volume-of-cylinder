5969f4e002a82f55738e2969989e2e84fc2f159b
Prashant Birajdar <92636189+Superprashant@users.noreply.github.com>
2021-11-22 17:20:08 +0530

    Add files via upload

---
+++ volume of cylinder.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main()
+{int r,h;
+cout<< "Insert the value of radius : ";
+cin>>r;
+cout<< "Insert the value of height : ";
+cin>>h;
+cout<<"volume of cylinder is: "<<3.14*r*r*h;
+return 0;
+}
